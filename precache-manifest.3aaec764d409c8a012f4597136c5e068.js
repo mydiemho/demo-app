b4b44208a211c5acc71cc0579693f878019aadab
My Ho <mydiemho@gmail.com>
2019-02-05 23:34:55 -0800

    Updates

---
+++ precache-manifest.3aaec764d409c8a012f4597136c5e068.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "28fc3e86c9ea21c389e9",
+    "url": "/demo-app/static/js/runtime~main.28fc3e86.js"
+  },
+  {
+    "revision": "cf301fb1b61888a56536",
+    "url": "/demo-app/static/js/main.cf301fb1.chunk.js"
+  },
+  {
+    "revision": "5a0bd862c79e48328471",
+    "url": "/demo-app/static/js/1.5a0bd862.chunk.js"
+  },
+  {
+    "revision": "cf301fb1b61888a56536",
+    "url": "/demo-app/static/css/main.8c681ec5.chunk.css"
+  },
+  {
+    "revision": "7033ea2b0b7cdf56d3d6d22ed3b8026f",
+    "url": "/demo-app/index.html"
+  }
+];
