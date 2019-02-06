ba32ba28c2368f8d2933b0dc1df6f6a41281aaa0
My Ho <mydiemho@gmail.com>
2019-02-05 22:37:32 -0800

    add button that does nothing

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -3,9 +3,11 @@ import './Hello.css';
 export interface IProps {
     name: string;
     enthusiasmLevel?: number;
+    onIncrement?: () => void;
+    onDecrement?: () => void;
 }
 
-function Hello({ name, enthusiasmLevel = 1 }: IProps) {
+function Hello({ name, enthusiasmLevel = 1, onIncrement, onDecrement }: IProps) {
     if (enthusiasmLevel <= 0) {
         throw new Error('You could be a little more enthusiastic. :D');
     }
@@ -15,6 +17,8 @@ function Hello({ name, enthusiasmLevel = 1 }: IProps) {
             <div className="greeting">
                 Hello {name + getExclamationMarks(enthusiasmLevel)}
             </div>
+            <button onClick={onDecrement}>-</button>
+            <button onClick={onIncrement}>+</button>
         </div>
     );
 }
